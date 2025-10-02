305a32d0214de72318190a2f98a0cc604b801ebf
alyho <alyssachangho@gmail.com>
2025-10-02 18:07:26 -0400

    maze, doorbell puzzle added

--- Assets/Scripts/Interactions/Interactable.cs
+++ Assets/Scripts/Interactions/Interactable.cs
@@ -6,13 +6,19 @@ using UnityEngine.Serialization;
 public class Interactable : MonoBehaviour
 {
     private Outline _outlineScript;
+    private float mouseDownTime;
+    private float holdThreshold = 0.5f;
+    private bool isHolding;
 
     [Tooltip("The maximum distance at which this is interactable")]
     public float maxDistance = 15f;
 
     public UnityEvent onClick;
+    public UnityEvent onHold;
+    public UnityEvent onHoldRelease;
 
-    [FormerlySerializedAs("interactable")] [Tooltip("Use this to toggle interactibility")]
+    [FormerlySerializedAs("interactable")]
+    [Tooltip("Use this to toggle interactibility")]
     public bool isInteractible = true;
 
     private void Awake()
@@ -24,10 +30,34 @@ public class Interactable : MonoBehaviour
     public void Update()
     {
         bool currentlyHighlighted = PointAtInteractable.CurrentlyHighlightedObject == gameObject;
-        
+
         _outlineScript.enabled = isInteractible && currentlyHighlighted;
         if (currentlyHighlighted && Input.GetMouseButtonDown(0))
-            OnClick();
+        {
+            mouseDownTime = Time.time;
+            isHolding = false;
+        }
+
+        if (currentlyHighlighted && Input.GetMouseButton(0))
+        {
+            if (!isHolding && Time.time - mouseDownTime >= holdThreshold)
+            {
+                isHolding = true;
+                OnHold();
+            }
+        }
+        if (currentlyHighlighted && Input.GetMouseButtonUp(0))
+        {
+            if (!isHolding)
+            {
+                OnClick();
+            }
+            else
+            {
+                OnHoldRelease();
+            }
+        }
+
     }
 
     public void SetInteractable(bool interactable)
@@ -40,5 +70,20 @@ public class Interactable : MonoBehaviour
         if (!isInteractible) return;
 
         onClick.Invoke();
+    }
+
+    private void OnHold()
+    {
+        if (!isInteractible) return;
+        Debug.Log("Mouse Held");
+
+        onHold.Invoke();
+    }
+
+    private void OnHoldRelease()
+    {
+        if (!isInteractible) return;
+        Debug.Log("Hold Released");
+        onHoldRelease.Invoke();
     }
 }
