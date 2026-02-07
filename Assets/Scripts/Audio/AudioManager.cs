a727a478110ee3bfe43f97e94404f74fb623044d
alyho <alyssachangho@gmail.com>
2026-02-07 13:22:24 -0500

    turn down sound

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using UnityEngine.Audio;
 
+
 public class AudioManager : MonoBehaviour
 {
     public AudioMixer masterMixer;
 
     public static AudioManager Instance = null;
+
+    //private AudioSource music; 
 	
     private void Awake()
     {
+
+        //music = gameObject.GetComponent<AudioSource>(); 
+
         if (Instance == null)
         {
             Instance = this;
@@ -25,4 +31,25 @@ public class AudioManager : MonoBehaviour
     {
         masterMixer.SetFloat("masterVol", Mathf.Log(soundLevel) * 20);
     }
+
+    /*public void AdjustMusicVolume(float duration, float targetVolume){
+        StartCoroutine(FadeVolume(duration, targetVolume)); 
+    }
+
+    private static IEnumerator FadeVolume(float duration, float targetVolume)
+    {
+        float startVolume = audioSource.volume;
+        float currentTime = 0;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, targetVolume, currentTime / duration);
+            yield return null; // Wait for the next frame
+        }
+
+        audioSource.volume = targetVolume;
+    }*/
+
+
 }
